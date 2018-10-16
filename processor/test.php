7a610690ea822ae551d327f6e8938d14f61be3af
jspeedz <wimjoris@gmail.com>
2018-10-16 22:14:48 +0200

    Little bit more sensible example

--- processor/test.php
+++ processor/test.php
@@ -8,11 +8,27 @@ class Test extends Processor {
 	}
 
 	public function process() {
-//		throw new \Exception('x');
-		foreach($this->getData(``) as $item) {
-			sleep(1);
-//			sleep(rand(1, 2));
-//			echo $item . PHP_EOL;
+		foreach($this->getData(``) as $randomMd5Hash) {
+			if(version_compare(PHP_VERSION, '7.2.0') >= 0 && defined('PASSWORD_ARGON2I')) {
+				$algorithm = PASSWORD_ARGON2I;
+				$options = [];
+			}
+			else {
+				$algorithm = PASSWORD_BCRYPT;
+				$options = [
+					'cost' => rand(4, 10),
+				];
+			}
+
+			$hashedString = password_hash($randomMd5Hash, $algorithm, $options);
+
+			if(password_needs_rehash($hashedString, $algorithm)) {
+				// Will never happen between 3 lines up and here, ha-ha
+			}
+
+			if(!password_verify($randomMd5Hash, $hashedString)) {
+				// Will never not verify
+			}
 		}
 	}
 
@@ -23,6 +39,6 @@ class Test extends Processor {
 	 * Exits the current process, with optional exit value.
 	 */
 	public function exit() {
-		exit(1);
+		exit(0);
 	}
 }
--- test/processor.php
+++ test/processor.php
@@ -7,11 +7,19 @@ require_once __DIR__ . '/../processor/test.php';
 use MultiProcessor\MultiProcessor;
 use MultiProcessor\Utility\ProcessorCores;
 
-$data = new \MultiProcessor\Iterator\ArrayIterator(array_merge(array_fill(0, 100, md5(microtime())), ['2', '3', '4', '5', '6', '7', 'u', 'v', 'w', 'x', 'y', 'z']));
+$randomHashGenerator = function() {
+	return md5(microtime());
+};
+
+$array = [];
+for($i = 0; $i < 1000; $i++) {
+	$array[] = $randomHashGenerator();
+}
+
+$data = new \MultiProcessor\Iterator\ArrayIterator($array);
 $processor = new \MultiProcessor\Processor\Test();
 
 $multiProcessor = new MultiProcessor($data, $processor);
-//$multiProcessor->setMaximumConcurrentChildren(ProcessorCores::getNumberOfCores());
-$multiProcessor->setMaximumConcurrentChildren(4);
-$multiProcessor->setChunkSize(2);
+$multiProcessor->setMaximumConcurrentChildren(ProcessorCores::getNumberOfCores());
+$multiProcessor->setChunkSize(5);
 $multiProcessor->run();
